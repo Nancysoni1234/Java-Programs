2589701c97f1920a2928bb47f49f1fad2e7d3ee8
Nancy Soni <ns1915@srmist.edu.in>
2025-01-29 09:04:33 +0530

    Differnt Patterns

---
+++ InvertedHalf.java
@@ -0,0 +1,92 @@
+// 1.Solid Rectangle
+// public class solicRec
+// {
+//   public static void main(String args[])
+//   {
+//     for(int i=0; i<5; i++)
+//     {
+//       for (int j=0; j<10; j++)
+//       {
+//         System.out.print("* ");
+//       }
+//       System.out.println("\n");
+//     }
+//   }
+// };
+
+//2.Hollow Rectangle
+// class hollowRec
+// {
+//     public static void main(String agrs[])
+// {
+//     for(int i=0; i<=4; i++)
+//     {
+//         for(int j=1; j<=5; j++)
+//         {
+//             if(i==0 || i==4 || j==1 ||j==5)
+//             {
+//                 System.out.print("* ");
+//             }
+//             else
+//             {
+//                 System.out.print("  ");
+//             }
+//         }
+//         System.out.println("\n");
+//     }
+// }
+// }
+
+
+//3.Half Payramid
+// public class halfPyramid
+// {
+//   public static void main(String agrs[])
+//   {
+//     for(int i=0; i<4; i++)
+//     {
+//       for(int k=0; i>=k; k++)
+//       {
+//         System.out.print("* ");
+//       }
+//       System.out.print("\n");
+//     }
+//   }
+// }
+
+//4.Inverted Half Pyramid
+// public class Inverted
+// {
+//   public static void main(String agrs[])
+//   {
+//     for(int i=4; i>0; i--)
+//     {
+//       for(int k=0; k<i; k++)
+//       {
+//         System.out.print("* ");
+//       }
+//       System.out.print("\n");
+//     }
+//   }
+// }
+
+//5.Inverted Half Rotated by 180 degree
+public class patterns
+{
+  public static void main(String agrs[])
+  {
+    for(int i=0; i<4; i++)
+    {
+      for(int j=3; j>i; j--)
+      {
+        System.out.print("  ");
+      }
+      for(int k=0; k<=i; k++)
+        {
+          System.out.print("* ");
+        }
+      System.out.print("\n");
+    }
+  }
+  
+}
