e6ac9348727f021f9657b1dba03651f8c81221cc
Nancy Soni <ns1915@srmist.edu.in>
2025-01-30 22:24:27 +0530

    Added 3 more patterns

--- InvertedHalf.java
+++ InvertedHalf.java
@@ -71,22 +71,93 @@
 // }
 
 //5.Inverted Half Rotated by 180 degree
-public class patterns
-{
-  public static void main(String agrs[])
-  {
-    for(int i=0; i<4; i++)
-    {
-      for(int j=3; j>i; j--)
-      {
-        System.out.print("  ");
-      }
-      for(int k=0; k<=i; k++)
-        {
-          System.out.print("* ");
-        }
-      System.out.print("\n");
-    }
-  }
+// public class patterns
+// {
+//   public static void main(String agrs[])
+//   {
+//     for(int i=0; i<4; i++)
+//     {
+//       for(int j=3; j>i; j--)
+//       {
+//         System.out.print("  ");
+//       }
+//       for(int k=0; k<=i; k++)
+//         {
+//           System.out.print("* ");
+//         }
+//       System.out.print("\n");
+//     }
+//   }
   
-}
+// }
+//5.Inverted Half Rotated by 180 degree
+// public class InvertedHalf
+// {
+//   public static void main(String agrs[])
+//   {
+//     for(int i=0; i<4; i++)
+//     {
+//       for(int j=3; j>i; j--)
+//       {
+//         System.out.print("  ");
+//       }
+//       for(int k=0; k<=i; k++)
+//         {
+//           System.out.print("* ");
+//         }
+//       System.out.print("\n");
+//     }
+//   }
+  
+// }
+//6.Half Payramid with numbers
+// public class halfPywithNum
+// {
+//   public static void main(String agrs[])
+//   {
+//     for(int i=1; i<=5; i++)
+//     {
+//       for(int j=1; j<=i; j++)
+//       {
+//         System.out.print(j);
+//       }
+//       System.out.print("\n");
+//     }
+//   }
+  
+// }
+
+//7.Inverted half Pyramid with numbers
+// public class IhalfPywithNum
+// {
+//   public static void main(String agrs[])
+//   {
+//     for(int i=5; i>=1; i--)
+//     {
+//       for(int j=1; j<=i; j++)
+//       {
+//         System.out.print(j);
+//       }
+//       System.out.print("\n");
+//     }
+//   }
+// }
+
+//8.floyd's Triangle
+// public class floyd
+// {
+//   public static void main(String agrs[])
+//   {
+//     int num = 1;
+//     for(int i=1; i<=5; i++)
+//     {
+//       for(int k=1; k<=i; k++)
+//       {
+//         System.out.print(num);
+//         System.out.print(" ");
+//         num++;
+//       }
+//       System.out.println("\n");
+//     }
+//   }
+// }
