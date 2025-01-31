43d1815b5377e97462a9386c5d50a2dd25451a45
Nancy Soni <ns1915@srmist.edu.in>
2025-01-31 23:37:40 +0530

    added more patterns

--- InvertedHalf.java
+++ InvertedHalf.java
@@ -161,3 +161,70 @@
 //     }
 //   }
 // }
+
+//9. 0-1 Triangle
+// public class zero_one
+// {
+//   public static void main(String agrs[])
+//   { 
+//     for(int i=1; i<=5; i++)
+//     {
+//       for(int k=1; k<=i; k++)
+//       { 
+//         if((i+k)%2==0)
+//         {
+//           System.out.print("1");
+//         }
+//         else
+//         {
+//           System.out.print("0");
+//         }
+//       }
+//       System.out.println("\n");
+//     }
+//   }
+// }
+
+//10.butterfly Pattern
+// public class butterfly1
+// {
+//   public static void main(String agrs[])
+//   { 
+//     int sub=6;
+//     for(int i=1; i<=4; i++)
+//     {
+//       for(int j=1; j<=i; j++)
+//       {
+//         System.out.print("* ");
+//       }
+//       for(int k=1; k<=sub; k++)
+//         {
+//           System.out.print("  ");
+//         }
+//         sub=sub-2;
+//       for(int l=1; l<=i; l++)
+//       {
+//         System.out.print("* ");
+//       }  
+//       System.out.println("\n");
+//     }
+//      int sub2=0;
+//     for(int m=0; m<4; m++)
+//     {
+//       for(int n=4; n>m; n--)
+//       {
+//         System.out.print("* ");
+//       }
+//       for(int p=0; p<sub2; p++)
+//       {
+//         System.out.print("  ");
+//       }
+//         sub2=sub2+2;
+//       for(int q=4; q>m; q--)
+//       {
+//         System.out.print("* ");
+//       }
+//       System.out.println("\n");
+//     }
+//   }
+// }   
